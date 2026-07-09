ab7fd2e8ac50b72dbd6c13890c3458debfe29961
Kittipat Tatsanakit <kittipat@pmodern.co.th>
2026-07-09 09:23:23 +0700

    Show full product photos

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = "event-pos-v13";
-const ASSETS = ["./", "./index.html", "./styles.css?v=13", "./app.js?v=13", "./manifest.webmanifest", "./icon.svg"];
+const CACHE_NAME = "event-pos-v14";
+const ASSETS = ["./", "./index.html", "./styles.css?v=14", "./app.js?v=14", "./manifest.webmanifest", "./icon.svg"];
 
 self.addEventListener("install", (event) => {
   event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
